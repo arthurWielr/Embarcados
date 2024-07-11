06336de4f3642b28bd3f5103bf1b0d5761ad655b
arthurWielr <149120972+arthurWielr@users.noreply.github.com>
2024-07-11 20:27:41 -0300

    Ok
    Ok

--- sample_project/main/main.c
+++ sample_project/main/main.c
@@ -18,83 +18,84 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include "freertos/queue.h"
+#include "freertos/timers.h"
 #include "driver/gpio.h"
 
-
-#define GPIO_OUTPUT_IO_0    02
-
+#define GPIO_OUTPUT_IO_0    2   // Defina o pino de saída conforme necessário
 #define GPIO_OUTPUT_PIN_SEL  (1ULL<<GPIO_OUTPUT_IO_0)
 #define GPIO_INPUT_IO_0    21
 #define GPIO_INPUT_IO_1    22
 #define GPIO_INPUT_IO_2    23
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1) | (1ULL<<GPIO_INPUT_IO_2))
-//#define GPIO_INPUT_PIN_CHANGE   ((1ULL<<GPIO_INPUT_IO_2))
 #define ESP_INTR_FLAG_DEFAULT 0
-
-
 
 static const char* TAG = "SYS INFO"; 
 
 static QueueHandle_t gpio_evt_queue = NULL; /* Queue handler */
+static TimerHandle_t mudanca_estado_timer = NULL;
+static bool mudanca_estado = false; // Variável global para controlar a mudança de estado
+static bool estado = false; // Estado atual do LED (true: aceso, false: apagado)
 
-static void IRAM_ATTR gpio_isr_handler(void* arg){
-    uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);  /* Definição do block pela queue e pelo numero do da porta */
+void IRAM_ATTR gpio_isr_handler(void* arg) {
+    uint32_t gpio_num = (uint32_t)arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);  /* Envia o número do GPIO para a fila */
 }
 
-static void gpio_task(void* arg){
+void mudanca_estado_timer_callback(TimerHandle_t xTimer) {
+    mudanca_estado = false; // Permite mudanças de estado novamente
+}
+
+static void gpio_task(void* arg) {
     uint32_t io_num;
-    for(;;) {
-        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) { /* Detecção do evento na porta, determinação da task */
-            static bool mudanca_estado = false;
+    for (;;) {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) { /* Detecção do evento na porta, determinação da task */
             bool acende = true;
             bool apaga = false;
-            bool estado =  false; //True aceso, false apagado;
-            ESP_LOGI(TAG,"YAY \n");
-            if(io_num == 21){ //Botão 1 - Acender
+
+            ESP_LOGI(TAG, "YAY \n");
+            if (io_num == GPIO_INPUT_IO_0) { // Botão 1 - Acender
                 estado = true;
                 gpio_set_level(GPIO_OUTPUT_IO_0, acende);
-                ESP_LOGI(TAG,"Acendeu")
-            } 
-            if(io_num == 22){ // Botão 2 - Apagar
+                ESP_LOGI(TAG, "Acendeu");
+            } else if (io_num == GPIO_INPUT_IO_1) { // Botão 2 - Apagar
                 estado = false;
                 gpio_set_level(GPIO_OUTPUT_IO_0, apaga);
-                ESP_LOGI(TAG,"Apagou")
-            } 
-
-            if (io_num == 23) { // Botão 3 - Switch
+                ESP_LOGI(TAG, "Apagou");
+            } else if (io_num == GPIO_INPUT_IO_2) { // Botão 3 - Switch
                 if (!mudanca_estado) { // Se o LED não estiver em transição
                     mudanca_estado = true; // Indica que o LED está em transição
-                    
+
                     if (estado == true) { // Se o LED está aceso, apaga-o
                         estado = false;
                         gpio_set_level(GPIO_OUTPUT_IO_0, apaga);
-                        ESP_LOGI(TAG,"Aceso -> Apagado")
+                        ESP_LOGI(TAG, "Aceso -> Apagado");
                     } else { // Se o LED está apagado, acende-o
                         estado = true;
                         gpio_set_level(GPIO_OUTPUT_IO_0, acende);
-                        ESP_LOGI(TAG,"Apagado -> Aceso")
+                        ESP_LOGI(TAG, "Apagado -> Aceso");
                     }
-                    // Define um timer para limpar o estado de mudança após um curto período
-                    xTimerStartOneShot(mudanca_estado_timer, pdMS_TO_TICKS(100)); // Timer de 100 ms
+
+                    // Inicia o temporizador para limpar o estado de mudança após um curto período
+                    xTimerStart(mudanca_estado_timer, 0); // Temporizador de 1000 ms
                 }
             }
         } 
     }
 }
 
+void app_main(void) {
+    // Cria uma fila para lidar com eventos GPIO a partir da ISR
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    mudanca_estado_timer = xTimerCreate("mudanca_estado_timer", pdMS_TO_TICKS(500), pdFALSE, (void*)0, mudanca_estado_timer_callback);
 
-
-void app_main(void){
-    
-    ESP_LOGI(TAG,"Hello world!\n");
+    ESP_LOGI(TAG, "Hello world!\n");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
     uint32_t flash_size;
     esp_chip_info(&chip_info);
-    
-    ESP_LOGI(TAG,"This is %s chip with %d CPU core(s), mmodel %d, WiFi%s%s, ",
+
+    ESP_LOGI(TAG, "This is %s chip with %d CPU core(s), model %d, WiFi%s%s, ",
            CONFIG_IDF_TARGET,
            chip_info.cores,
            chip_info.model,
@@ -103,10 +104,10 @@ void app_main(void){
 
     unsigned major_rev = chip_info.revision / 100;
     unsigned minor_rev = chip_info.revision % 100;
-    ESP_LOGI(TAG,"silicon revision v%d.%d, ", major_rev, minor_rev);
+    ESP_LOGI(TAG, "silicon revision v%d.%d, ", major_rev, minor_rev);
 
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        ESP_LOGE(TAG,"Get flash size failed");
+    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        ESP_LOGE(TAG, "Get flash size failed");
         return;
     }
 
@@ -119,33 +120,26 @@ void app_main(void){
     gpio_config(&out_conf);
 
     gpio_config_t in_conf = {};
-    in_conf.intr_type = GPIO_INTR_ANYEDGE; /* Desabilita a interrupção do sistema */
+    in_conf.intr_type = GPIO_INTR_ANYEDGE; /* Configura interrupção em ambas as bordas */
     in_conf.mode = GPIO_MODE_INPUT;
     in_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     in_conf.pull_down_en = 0;
     in_conf.pull_up_en = 1;
     gpio_config(&in_conf);
 
-    //create a queue to handle gpio event from isr
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    //start gpio task
-    xTaskCreate(gpio_task, "gpio_task", 2048, NULL, 10, NULL);  /* Inicializa a task*/
+    // Inicializa a task GPIO
+    xTaskCreate(gpio_task, "gpio_task", 2048, NULL, 10, NULL);
 
-    //install gpio isr service
+    // Instala o serviço de interrupção GPIO
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    //hook isr handler for specific gpio pin
+    // Adiciona manipuladores de interrupção para pinos específicos
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
-
     gpio_isr_handler_add(GPIO_INPUT_IO_2, gpio_isr_handler, (void*) GPIO_INPUT_IO_2);
 
+    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
 
-    printf("Minimum free heap size: %"PRIu32" bytes\n", esp_get_minimum_free_heap_size());
-      
-       
-    while(1) {
+    while (1) {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-
 }
